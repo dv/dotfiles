e80f0689edcb2d29b66c2947309b4105a9215c60
David Verhasselt <david@crowdway.com>
2015-05-10 22:31:28 +0300

    UTF-8 all the things!

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -7,6 +7,10 @@ fi
 export LSCOLORS="exfxcxdxbxegedabagacad"
 export CLICOLOR=true
 
+# UTF-8 all the things!
+export LC_ALL="en_US.UTF-8"
+export LANG="en_US.UTF-8"
+export LANGUAGE="en_US.UTF-8"
 
 
 fpath=($DOTFILES_ROOT/functions $fpath)
