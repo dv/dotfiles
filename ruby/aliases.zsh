a041c8e1709895d03d735d69b697fcd75ebe8d9a
David Verhasselt <david@crowdway.com>
2015-05-10 22:37:50 +0300

    Add ruby aliases

--- ruby/aliases.zsh
+++ ruby/aliases.zsh
@@ -1,3 +1,16 @@
 alias sc='script/console'
 alias sg='script/generate'
 alias sd='script/destroy'
+
+# Be. Excited. Be! Be! Excited!
+alias be="bundle exec"
+
+# Grep rails routes
+alias rgrep="bundle exec rake routes | grep"
+
+# Run migrate, rollback, and migrate again. This is a good test
+# to check if the migration works in both directions. Thanks Bikeshed.fm!
+alias remigrate="bundle exec rake db:migrate && bundle exec rake db:rollback && bundle exec rake db:migrate"
+
+# Open all generated files in Sublime
+alias rg="~/scripts/sublime/rails_g_and_open.rb"
