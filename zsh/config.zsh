9df67455100a8e06f5524e7046aeeff842ba5d1f
David Verhasselt <david@crowdway.com>
2015-05-10 22:32:23 +0300

    Extra lazyness by cd-ing to dir without typing cd

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -33,6 +33,7 @@ setopt EXTENDED_HISTORY # add timestamps to history
 setopt PROMPT_SUBST
 setopt CORRECT
 setopt COMPLETE_IN_WORD
+setopt AUTOCD # go into directory without typing cd
 
 setopt APPEND_HISTORY # adds history
 setopt INC_APPEND_HISTORY SHARE_HISTORY  # adds history incrementally and share it across sessions
