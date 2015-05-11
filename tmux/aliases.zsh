a83d78cd7be7ffb5a842102deedb65cbf1a83137
David Verhasselt <david@crowdway.com>
2015-05-11 12:23:17 +0300

    Add cmd `tclear` to clear all naked panes in tmux

--- tmux/aliases.zsh
+++ tmux/aliases.zsh
@@ -1,3 +1,4 @@
 # Make commands with arguments work such as "subl /tmp"
 # Requires reattach-to-user-namespace under tmux
 alias subl="reattach-to-user-namespace /usr/local/bin/subl"
+alias tclear="run_in_all_naked_panes clear"
