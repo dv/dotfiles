dea7f8d7dbd17c1737af315cdb97dc67b3ce235e
David Verhasselt <david@crowdway.com>
2016-01-31 19:47:05 +0200

    Fix oh-my-zsh installation

--- oh-my-zsh/install.sh
+++ oh-my-zsh/install.sh
@@ -1,4 +1,4 @@
-if [ ! -d "~/.oh-my-zsh" ]; then
+if [ ! -d ~/.oh-my-zsh ]; then
   echo "  Installing oh-my-zsh for you."
 
   sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
