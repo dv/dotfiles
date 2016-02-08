86ffa08be5465bb75321296d221fff7f03cc232a
David Verhasselt <david@crowdway.com>
2016-02-08 10:46:16 +0200

    Install `subl` command line tool

--- sublime/install.sh
+++ sublime/install.sh
@@ -2,6 +2,9 @@
 # Setup a machine for Sublime Text 3
 set -x
 
+# Install `subl` command line tool
+ln -s "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" /usr/local/bin/subl
+
 # Symlink settings
 mv ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User.backup
 ln -s $DOTFILES_ROOT/sublime/User ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
