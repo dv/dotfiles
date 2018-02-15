d2a99f63faf1f8747e1413bb79c72b227614664e
David Verhasselt <david@crowdway.com>
2018-02-15 17:49:06 +0200

    Add some git aliases

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -17,3 +17,7 @@ alias gco='git checkout'
 alias gcb='git copy-branch-name'
 alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
+alias rebase='git fetch && git rebase origin/master'
+
+# Run rubocop on all files that are changed versus master, thanks @mrfoto
+alias lazycop='git diff-tree -r --no-commit-id --name-only master HEAD | xargs rubocop'
